be4bd5d15c500954754a968eede8126437a09a0a
Abdulbaki Suraj <codelikesuraj@gmail.com>
2024-09-09 09:53:03 +0100

    10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (index) {
+    if (index < 0) {
+        return 'OOPS';
+    }
 
+    if (index == 0) {
+        return 0;
+    }
+
+    let previousValue = 0;
+    let fibonacciValue = 1;
+
+    for (let i = 1; i < index; i++) {
+        [fibonacciValue, previousValue] = [fibonacciValue + previousValue, fibonacciValue];
+    }
+
+    return fibonacciValue;
 };
 
 // Do not edit below this line
