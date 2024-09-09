fb9f2b3644d782462e148be80fe8a889ce59127e
Abdulbaki Suraj <codelikesuraj@gmail.com>
2024-09-09 08:44:16 +0100

    08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((total, number) => total += number, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce((total, number) => total *= number, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  if (n < 1) {
+    return 1;
+  }
+
+  let factorial = 1;
+  for (i = n; i > 0; i--) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
