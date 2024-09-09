c77cd804fbd19ef54c20daec9c5c73fe9fa1be1a
Abdulbaki Suraj <codelikesuraj@gmail.com>
2024-09-09 11:38:42 +0100

    12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const getAge = function (person) {
+    return (person.yearOfDeath ??= new Date().getFullYear()) - person.yearOfBirth;
+}
 
+const findTheOldest = function (people) {
+    return people.toSorted((a, b) => {
+        if (getAge(a) > getAge(b)) {
+            return -1;
+        } else {
+            return 1;
+        }
+    })[0];
 };
 
 // Do not edit below this line
